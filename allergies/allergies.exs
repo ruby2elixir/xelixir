d694edf7e8cc76f4869fa900e6630bd86fac0937
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-23 12:42:28 +0100

    allergies solved

--- allergies/allergies.exs
+++ allergies/allergies.exs
@@ -1,10 +1,14 @@
 defmodule Allergies do
+  @allergies ~w[eggs peanuts shellfish strawberries tomatoes chocolate pollen cats]
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
   @spec list(non_neg_integer) :: [String.t]
   def list(flags) do
-  
+    List.zip([presence_bits(flags), @allergies])
+      |> Enum.filter( fn(x)-> elem(x, 0) == 1 end )
+      |> Enum.map( fn(x)-> elem(x, 1) end)
   end
 
   @doc """
@@ -12,6 +16,11 @@ defmodule Allergies do
   """
   @spec allergic_to?(non_neg_integer, String.t) :: boolean
   def allergic_to?(flags, item) do
-  
+    item in list(flags)
+  end
+
+  def presence_bits(flags) do
+    Integer.digits(flags, 2) |> Enum.reverse
   end
 end
+
---
+++ allergies/notes.md
@@ -0,0 +1,2 @@
+# did without Bitwise module,
+# need to learn bit-wise operations
