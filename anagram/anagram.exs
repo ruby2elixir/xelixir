91ad0bbcc7a52529a5feb3ff7053751d11c53372
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-23 13:30:48 +0100

    anagram example works 

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,6 +4,29 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    Enum.with_index(candidates)
+      |> Enum.filter( fn({el, idx})-> not is_equal(base, el) end)
+      |> Enum.filter( fn({el, idx})-> is_anagram(base, el) end)
+      |> Enum.map( fn({el, idx})-> el end )
+  end
+
+  def sort_chars(str) do
+    str
+      |> String.downcase
+      |> String.to_char_list
+      |> Enum.sort
+      |> List.to_string
+  end
+
+  def is_equal(base, candidate) do
+    normalize(base) == normalize(candidate)
+  end
+
+  def is_anagram(base, candidate) do
+    sort_chars(base) == sort_chars(candidate)
+  end
+
+  def normalize(str) do
+    str |> String.downcase
   end
 end
