309458caa059c456a6008d535f272487364d1925
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-23 22:57:12 +0100

    bob finished

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,8 +1,24 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+        silence?(input)  -> "Fine. Be that way!"
+        shouting?(input) -> "Whoa, chill out!"
+        question?(input) -> "Sure."
+        true ->  "Whatever."
     end
   end
+
+  def shouting?(str) do
+    (String.upcase(str) == str) && letters?(str)
+  end
+
+  def question?(str) do
+    String.at(str, -1) == "?"
+  end
+
+  def silence?(str) do
+    String.strip(str) == ""
+  end
+
+  defp letters?(input),  do: Regex.match?(~r/\p{L}+/, input)
 end
