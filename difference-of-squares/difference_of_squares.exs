99d953ee802794111b6eb981cf5087b763ea7090
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-24 13:44:32 +0100

    sum_of_squares

--- difference-of-squares/difference_of_squares.exs
+++ difference-of-squares/difference_of_squares.exs
@@ -7,24 +7,24 @@ defmodule Squares do
   Calculate sum of squares from 1 to a given end number.
   """
   @spec sum_of_squares(pos_integer) :: pos_integer
-  def sum_of_squares(number) do
+  def sum_of_squares(number),       do: sum_of_squares(number, 0)
+  def sum_of_squares(0, acc),       do: acc
+  def sum_of_squares(number, acc),  do: sum_of_squares(number-1, acc + (number*number))
 
-  end
 
   @doc """
   Calculate square of sums from 1 to a given end number.
   """
   @spec square_of_sums(pos_integer) :: pos_integer
-  def square_of_sums(number) do
-
-  end
+  def square_of_sums(number),       do: square_of_sums(number, 0)
+  def square_of_sums(0, acc),       do: acc * acc
+  def square_of_sums(number, acc),  do: square_of_sums(number-1, acc + number)
 
   @doc """
   Calculate difference between sum of squares and square of sums from 1 to a given end number.
   """
   @spec difference(pos_integer) :: pos_integer
   def difference(number) do
-
+    square_of_sums(number) - sum_of_squares(number)
   end
-
 end
---
+++ difference-of-squares/notes.md
@@ -0,0 +1,2 @@
+I chose to solve the example with recursion, it does read quite nicely.
+I think my solution is a bit more readable
